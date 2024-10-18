7f061b62e79c6b2f0a5fbde24808ef1451d2477b
masterzero1993 <cumiquirdavid@gmail.com>
2024-10-18 14:02:28 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,11 +1,24 @@
 <?php
-$destino = "cumiquirdavid@gmail.com";
-$nombre = $_POST['nombre'];
-$correo = $_POST['email'];
-$telefono = $_POST['tel'];
-$mensaje = $_POST['mensaje'];
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
-$contenido = "nombre: " .$nombre . "\nCorreo: " . $correo . "\ntelefono: " . $telefono . "\nMensaje: " .$mensaje;
-mail($destino,"Contacto", $contenido);
-header("location:index.html");
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'cumiquirdavid@gmail.com';
+$asunto = 'Mensaje de... (Escribe como quieres que se vea el remitente de tu correo)';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
 ?>
