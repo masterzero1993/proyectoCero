231fc82c4f3855ee5128f3d11368a3ff661268ee
masterzero1993 <cumiquirdavid@gmail.com>
2024-10-19 00:16:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>CONTACTENOS</title>
-</head>
-<body>
-    <div>
-        <form action="validar.php" method="POST"><!--esa direccion es la que cree en esa pagina con mi correo para que redirija 
-            el formulario a mi correo-->
-            <input type="text" class="input" placeholder="nombre completo" name="nombre" ><br>
-            <input type="email" class="input" placeholder="Email" name="email" ><br>
-            <input type="tel" class="input" placeholder="numero de telefono" name="tel"><br>
-            <textarea id="mensaje" placeholder="escriba un mensaje" name="mensaje"></textarea><br>
-            <input type="submit" value="enviar" id="button">
-        </form>
-    </div>
-</body>
-</html>
+<?php
+echo'hola';
+
+
+?>
