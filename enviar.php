70fbfd1484881846123c0e7f1dbdb8e6e5c6a2f6
masterzero1993 <cumiquirdavid@gmail.com>
2024-10-18 23:25:01 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,10 +1,17 @@
-<?php
-$para      = 'cumiquirdavid@gmail.com';
-$titulo    = $_POST['nombre'];
-$mensaje   = $_POST['mensaje'];
-$cabeceras = 'From: laveronica_01@hotmail.com' . "\r\n" .
-    'Reply-To: laveronica_01@hotmail.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+ <?php
 
-mail($para, $titulo, $mensaje, $cabeceras);
-?>
+          $nombre=$_POST['nombre'];
+          $email=$_POST['email'];
+          $telefono=$_POST['tel'];
+          $mensaje=$_POST['mensaje'];
+        
+          $contenido="
+          Nombre: $nombre
+          Email: $email
+          Tel: $telefono
+          Mensaje: $mensaje
+          ";
+
+          $archivo=fopen("recibido/$email.txt","w");
+          fwrite($archivo,$contenido);
+        ?>
